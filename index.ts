6aaa00dba01a035c337274067e1cb0512928523d
Ridwan Budiman <ridwan.etlanda@mekari.com>
2025-04-21 18:02:30 +0700

    fix import

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 // src/AgentQStatusUpdater.ts
-import axios, { type AxiosInstance } from 'axios';
+import axios from 'axios';
+import type { AxiosInstance } from 'axios';
 
 type TestCase = {
   id: string;
